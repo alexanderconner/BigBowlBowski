4cc8c140e2fc3f08425459e9342c90d90ce04de7
alexanderconner <astralbody888@gmail.com>
2017-10-08 18:09:23 -0400

    added a GUI element that captures the number of pins standing, will be used for setting the score.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold; //Determines if pin is still standing.
+
+	//Returns true if the pins transform is rotated less than the threshold from vertical
+	public bool IsStanding() {
+
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+		//float tiltInX = Mathf.Abs(rotationInEuler.x);
+		//float tiltInZ = Mathf.Abs(rotationInEuler.z);
+		float tiltInX = Mathf.Abs(Mathf.DeltaAngle(transform.rotation.eulerAngles.x, 0));
+		float tiltInZ = Mathf.Abs(Mathf.DeltaAngle(transform.rotation.eulerAngles.z, 0));
+
+		if (tiltInX < standingThreshold && tiltInZ < standingThreshold) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text pinCountText;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pinCountText.text = CountStanding ().ToString();
+	}
+
+	public int CountStanding() {
+
+		int pinsStanding = 0;
+
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin> ()) {
+			if (pin.IsStanding()) {
+				pinsStanding += 1;
+			}
+		}
+		return pinsStanding;
+	}
+}
